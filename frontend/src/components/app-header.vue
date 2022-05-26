b5a0f384c44a92300b5a95457a65281886cffb27
Alexandre <clemencot.alexandre@gmail.com>
2022-05-26 16:47:00 +0200

    début du front + changement règles Eslint

---
+++ frontend/src/components/app-header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <router-link to="/">Accueil</router-link> |
+    <router-link to="/about">Le club</router-link>
+    <router-link to="/about">Calendrier</router-link>
+    <router-link to="/about">Evènements</router-link>
+    <router-link to="/about">Section sportive</router-link>
+    <router-link to="/about">Equipes</router-link>
+    <router-link to="/about">Multimédia</router-link>
+    <router-link to="/about">Contact</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "appHeader",
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: red;
+  height: 200px;
+}
+</style>
