c2c8448a0f057bc2bb2fd0ae8a2a003724b07421
Alexandre <clemencot.alexandre@gmail.com>
2022-07-05 23:39:23 +0200

    ajout de react router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,33 @@
 import React from "react";
 import "../style/components/_Header.scss";
+import Logo from "../assets/logo.JPG";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
 
 const Header = () => {
     return (
-      <div className="header">
-        <h1>FCB Le Bourget</h1>
-        <nav className="header_nav">
-          <ul className="header_nav_list">
-            <li className="header_nav_list_item">Accueil</li>
-            <li className="header_nav_list_item">Compétition</li>
-            <li className="header_nav_list_item">Galerie</li>
-            <li className="header_nav_list_item">Contact</li>
-          </ul>
-        </nav>
-      </div>
+      <Router>
+        <div className="header">
+          <img src={Logo} alt="logo du club de football" />
+          <h1>FCB Le Bourget</h1>
+          <nav className="header_nav">
+            <ul className="header_nav_list">
+              <li className="header_nav_list_item">
+                <Link to="/">Accueil</Link>
+              </li>
+              <li className="header_nav_list_item">
+                <Link to="/competition">Compétition</Link>
+              </li>
+              <li className="header_nav_list_item">
+                <Link to="/galerie">Galerie</Link>
+              </li>
+              <li className="header_nav_list_item">
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </Router>
     );
 };
 
