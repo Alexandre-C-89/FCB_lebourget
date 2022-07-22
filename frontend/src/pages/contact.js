c51b5ca713dac7bbcbf5d5a88d88e1c9f7208969
Alexandre <clemencot.alexandre@gmail.com>
2022-07-22 14:00:04 +0200

    refonte du style du formulaire

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -2,21 +2,32 @@ import "../style/components/_Contact.scss";
 
 function Contact() {
   return (
-    <div className="form">
+    <div className="box">
         <h2>Contactez le Club !</h2>
-        <form className="form_contact">
-          <label className="form_contact_name">
+        <form className="form_bloc">
+
+          <div className="form_bloc_group">
+            <label for="nom">
             Nom: 
-            <input type="text" name="Nom"/> 
-          </label>
-          <label className="form_contact_lastname">
-            Prénom: 
-            <input type="text" name="Prénom"/> 
-          </label>
-          <label className="form_contact_age">
-            Age: 
-            <input type="number" name="Age"/> 
-          </label>
+            </label>
+            <input type="text" name="Nom" required maxLength="20"/> 
+          </div>
+          
+          <div className="form_bloc_group">
+            <label for="prenom" >
+              Prénom: 
+            </label>
+              <input type="text" name="Prénom" required maxLength="20"/> 
+          </div>
+
+          <div className="form_bloc_group">
+            <label for="message">Message :</label>
+            <textarea name="message" id="message" cols="30" rows="10"></textarea>
+          </div>
+
+          <div className="form_bloc_group">
+            <input type="submit" value="Envoyer" className="button_sub"/>
+          </div>
         </form>
       </div>
   );
